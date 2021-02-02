89d41e17409db2821babe2465b6f05ee39de8cc6
ombhilare999 <ombhilare999@gmail.com>
2021-02-03 00:11:03 +0530

    Pll Added to convert 24 MHz to 25MHz for VGA standard

--- src/vga_test_pattern_top.v
+++ src/vga_test_pattern_top.v
@@ -14,7 +14,11 @@ module vga_test_pattern_top
 (
     //Main Clock
     input i_clk,
-    input [2:0]i_pattern,
+
+	//Select Input for Pattern Display
+	input sel0,
+	input sel1,	
+	
     //VGA
     output   o_VGA_hsync,
     output   o_VGA_vsync,
@@ -26,7 +30,8 @@ module vga_test_pattern_top
     output o_VGA_green_2,
     output  o_VGA_blue_0,
     output  o_VGA_blue_1,
-    output  o_VGA_blue_2
+    output  o_VGA_blue_2,
+    output reg o_VGA_CLK = 1'b1
 );
 
     //Parameter Needed:
@@ -35,9 +40,6 @@ module vga_test_pattern_top
     parameter TOTAL_ROWS  =  525;
     parameter ACTIVE_COLS =  640;
     parameter ACTIVE_ROWS =  480;  
-	
-	//Pattern for dispaly
-	//wire [2:0]i_pattern = 3'b001;
 
     // Common VGA Signals
     wire w_hsync_vga, wire_vsync_vga;
@@ -46,14 +48,41 @@ module vga_test_pattern_top
     wire [VIDEO_WIDTH-1:0]   w_red_TP,   w_red_Porch;
     wire [VIDEO_WIDTH-1:0] w_green_TP, w_green_Porch;
     wire [VIDEO_WIDTH-1:0]  w_blue_TP,  w_blue_Porch;
+	
+	// Clock Signals
+	
+	wire bufpll_lock, pclk;
 
+	ip_pll pll
+	(
+		.refclk		(i_clk),
+		.reset		(1'b0),
+		.extlock	(bufpll_lock),
+		.clk0_out	(pclk)
+	);
+	
+	//Pattern for dispaly
+	wire [2:0]i_pattern;
+    reg  [2:0]i_pattern_reg;
+
+	always @(sel0 or sel1) begin
+        case({sel0, sel1}) 
+            2'b00: i_pattern_reg <= 3'b000;
+            2'b01: i_pattern_reg <= 3'b001;
+            2'b10: i_pattern_reg <= 3'b010;
+            2'b11: i_pattern_reg <= 3'b011;
+        endcase 
+    end
+
+    assign i_pattern = i_pattern_reg;
+	
     //////////////////////////////////////////////////////////////////////////////////////
     // Creating Instants of VGA modules
     //////////////////////////////////////////////////////////////////////////////////////
 
     vga_sync_pulse i1
     (
-        .i_clk(i_clk),
+        .i_clk(pclk),
         .o_hsync(w_hsync_vga),
         .o_vsync(w_vsync_vga),
         .o_col_count(),
@@ -62,7 +91,7 @@ module vga_test_pattern_top
 
     test_pattern_gen i2
     (
-        .i_clk(i_clk),
+        .i_clk(pclk),
         .i_pattern(i_pattern),
         .i_hsync(w_hsync_vga),
         .i_vsync(w_vsync_vga),
@@ -75,7 +104,7 @@ module vga_test_pattern_top
 
     vga_sync_porch i4
     (
-        .i_clk(i_clk),
+        .i_clk(pclk),
         .i_hsync(w_hsync_tp),
         .i_vsync(w_vsync_tp),
         .i_red_video(w_red_TP),
@@ -87,6 +116,9 @@ module vga_test_pattern_top
         .o_green_video(w_green_Porch),
         .o_blue_video(w_blue_Porch)
     );
+	
+	always @(posedge pclk) 
+		o_VGA_CLK <= ~o_VGA_CLK;
 
     assign   o_VGA_hsync = w_hsync_porch;
     assign   o_VGA_vsync = w_vsync_porch;
@@ -102,4 +134,58 @@ module vga_test_pattern_top
     assign  o_VGA_blue_0 = w_blue_Porch[0];
     assign  o_VGA_blue_1 = w_blue_Porch[1];
     assign  o_VGA_blue_2 = w_blue_Porch[2]; 
+endmodule
+
+module ip_pll(refclk,
+		reset,
+		extlock,
+		clk0_out);
+
+	input refclk;
+	input reset;
+	output extlock;
+	output clk0_out;
+
+	wire clk0_buf;
+
+	EG_LOGIC_BUFG bufg_feedback( .i(clk0_buf), .o(clk0_out) );
+
+	EG_PHY_PLL #(.DPHASE_SOURCE("DISABLE"),
+		.DYNCFG("DISABLE"),
+		.FIN("24.000"),
+		.FEEDBK_MODE("NORMAL"),
+		.FEEDBK_PATH("CLKC0_EXT"),
+		.STDBY_ENABLE("DISABLE"),
+		.PLLRST_ENA("ENABLE"),
+		.SYNC_ENABLE("ENABLE"),
+		.DERIVE_PLL_CLOCKS("DISABLE"),
+		.GEN_BASIC_CLOCK("DISABLE"),
+		.GMC_GAIN(6),
+		.ICP_CURRENT(3),
+		.KVCO(6),
+		.LPF_CAPACITOR(3),
+		.LPF_RESISTOR(2),
+		.REFCLK_DIV(24),
+		.FBCLK_DIV(25),
+		.CLKC0_ENABLE("ENABLE"),
+		.CLKC0_DIV(30),
+		.CLKC0_CPHASE(30),
+		.CLKC0_FPHASE(0)	)
+	pll_inst (.refclk(refclk),
+		.reset(reset),
+		.stdby(1'b0),
+		.extlock(extlock),
+		.psclk(1'b0),
+		.psdown(1'b0),
+		.psstep(1'b0),
+		.psclksel(3'b000),
+		.psdone(open),
+		.dclk(1'b0),
+		.dcs(1'b0),
+		.dwe(1'b0),
+		.di(8'b00000000),
+		.daddr(6'b000000),
+		.do({open, open, open, open, open, open, open, open}),
+		.fbclk(clk0_out),
+		.clkc({open, open, open, open, clk0_buf}));
 endmodule
